42543f92db8bf0dc4be2923baf822561bdf4212e
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-01-31 19:33:05 +0500

    Toshlar oyini

--- robocontest_1.py
+++ robocontest_1.py
@@ -1,16 +1,16 @@
 # task-1
 # Azimjonning qo'ylari'
 
-# from roboContest import n
+from roboContest import n
 
-class SHeep_ears:
-    def countears_of_sheep(self, legs):
-        if legs % 4 == 0:
-            return legs // 2
-        else:
-            return -1
-
-es = SHeep_ears()
+# class SHeep_ears:
+#     def countears_of_sheep(self, legs):
+#         if legs % 4 == 0:
+#             return legs // 2
+#         else:
+#             return -1
+#
+# es = SHeep_ears()
 
 # print(es.countears_of_sheep(n))
 
@@ -21,18 +21,34 @@ es = SHeep_ears()
 # task-4
 # Sovg'a
 
-m=0
-g = 1
-gnomlarning_pullari = 0
-while m < 7:
-    m = int(input(f'{g}-gnomning puli qancha? '))
-    gnomlarning_pullari += m
-    g += 1
+# m=0
+# g = 1
+# gnomlarning_pullari = 0
+# while m < 7:
+#     m = int(input(f'{g}-gnomning puli qancha? '))
+#     gnomlarning_pullari += m
+#     g += 1
+#
+# gift = int(input('sovg\'a narxini kiriting: '))
+#
+# def solution(m, g):
+#     if m < 1000 and g < 1000:
+#         return g-m
 
-gift = int(input('sovg\'a narxini kiriting: '))
+# print(solution(gnomlarning_pullari, gift))
 
-def solution(m, g):
-    if m < 1000 and g < 1000:
-        return g-m
 
-print(solution(gnomlarning_pullari, gift))
+
+
+
+# task-5
+# Yig'indilar soni
+
+def winner(stone):
+    if 1 <= stone <= (10**9):
+        if stone % 2 == 0:
+            return 'Second Player'
+        else:
+            return 'First Player'
+
+print(winner(n))
