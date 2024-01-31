c9d392a7ec42973dd60c8e4901957959a8a1af87
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-01-31 19:07:53 +0500

    Sovga

--- robocontest_1.py
+++ robocontest_1.py
@@ -1,18 +1,38 @@
 # task-1
 # Azimjonning qo'ylari'
 
-from roboContest import n
+# from roboContest import n
 
 class SHeep_ears:
     def countears_of_sheep(self, legs):
-        try:
-            if legs > 0:
-                return int((legs / 4) * 2)
-            else:
-                return -1
-        except Exception as e:
-            return e
+        if legs % 4 == 0:
+            return legs // 2
+        else:
+            return -1
 
 es = SHeep_ears()
 
 # print(es.countears_of_sheep(n))
+
+
+
+
+
+# task-4
+# Sovg'a
+
+m=0
+g = 1
+gnomlarning_pullari = 0
+while m < 7:
+    m = int(input(f'{g}-gnomning puli qancha? '))
+    gnomlarning_pullari += m
+    g += 1
+
+gift = int(input('sovg\'a narxini kiriting: '))
+
+def solution(m, g):
+    if m < 1000 and g < 1000:
+        return g-m
+
+print(solution(gnomlarning_pullari, gift))
