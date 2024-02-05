b7dcd31bb9f829f5fbb1b2cb2ad0deaab2c4e6f9
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-02-06 01:38:31 +0500

    single-thread parsing

---
+++ Parse/single-thread.py
@@ -0,0 +1,24 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+
+start_time = time.time_ns() // 1_000_000
+
+for i in range(1, 16):
+    url = f'https://multimediya.uz/e-kitob/index.php?mavzu={i}'
+    respond = requests.get(url)
+
+try:
+    if respond.status_code == 200:
+        soup = BeautifulSoup(respond.content, 'html.parser')
+
+        for h in range(1, 16):
+            file = open(str(h)+'.html', 'w', encoding='UTF-8')
+            file.write(soup.prettify())
+            file.close()
+except Exception as e:
+    print(e)
+
+
+end_time = time.time_ns() // 1_000_000
+print(end_time - start_time) # 4583
